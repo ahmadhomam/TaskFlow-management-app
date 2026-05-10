58484b75b2b2221f915215f894b8fc472330896d
Homam Ahmad Kazimi <ahmadhomam38@gmail.com>
2026-05-11 03:50:38 +0530

    done minimum flask boots(ignitor)

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,31 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_socketio import SocketIO
+from dotenv import load_dotenv
+from flask_login import LoginManager
+import os
 
-socketio = SocketIO(cors_allowed_origins="*")
+load_dotenv()
+
+# Initialize extensions
+db = SQLAlchemy()
+login_manager = LoginManager()
+socketio = SocketIO()
 
 def create_app():
     app = Flask(__name__)
-    
-    # Initialize extensions
+    db.init_app(app)       # bound INSIDE create_app
+    login_manager.init_app(app)
     socketio.init_app(app)
+
+    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+    
+
+    from app.routes.auth import auth_bp
+    app.register_blueprint(auth_bp, url_prefix="/api/auth")
+
+    with app.app_context():
+        db.create_all()
     
     return app
